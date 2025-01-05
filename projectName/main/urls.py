b36d620ae5d8a064d822dae0c45f72a8b2625fe6
Shubham17112 <personal17122004@gmail.com>
2025-01-05 11:11:56 +0530

    commit

--- projectName/main/urls.py
+++ projectName/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Example view
     path('face/', include('face.urls')),  # This ensures all URLs starting with 'face/' are routed to face app
-
+    
+    
 ]
